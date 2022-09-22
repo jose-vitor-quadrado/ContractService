c6c8a17ef7dc74c65b62211384e92d4766191161
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-09-22 11:36:02 -0300

    Add project files.

---
+++ InterfaceFix/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using InterfaceFix.Entities;
+using InterfaceFix.Services;
+
+namespace InterfaceFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Data (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(contractNumber, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("\nInstallments:");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
